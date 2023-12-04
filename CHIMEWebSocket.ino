586c8eac6caaf13b824bbe822795e446b988bca2
tlm-design <149128509+tlm-design@users.noreply.github.com>
2023-12-04 11:36:06 -0800

    Add ability to control with 2 cap sensors

--- CHIMEWebSocket.ino
+++ CHIMEWebSocket.ino
@@ -2,6 +2,7 @@
   CHIMEWebSocket
   Created for CHIME prototype
   Control electromagnet with capacitive sensor
+  This version requires two cap sensors (two hands) to turn on electromagnet
   by Tara Manuel tmanuel@usc.edu
 */
 
@@ -10,10 +11,10 @@
 #include "arduino_secrets.h"
 #include <CapacitiveSensor.h>
 
-char ssid[] = SECRET_SSID;    // network ID stored in arduino_secrets.h
-char pass[] = SECRET_PASS;    // network password
-char serverAddress[] = SECRET_SERVER_ADDRESS;   // server address
-int port = 80;//3000;
+char ssid[] = SECRET_SSID;                     // network ID stored in arduino_secrets.h
+char pass[] = SECRET_PASS;                     // network password
+char serverAddress[] = SECRET_SERVER_ADDRESS;  // server address
+int port = 80;                                 //3000;
 
 String socketdata[3];
 int counter = 0;
@@ -24,47 +25,51 @@ WebSocketClient client = WebSocketClient(wifi, serverAddress, port);
 int status = WL_IDLE_STATUS;
 
 //sensing
-CapacitiveSensor capSensor = CapacitiveSensor(4, 2); // pin 4 sends electrical energy, pin 2 senses senses a change
-int threshold = 1000;
+CapacitiveSensor capSensor = CapacitiveSensor(4, 2);  // pin 4 sends electrical energy, pin 2 senses senses a change
+CapacitiveSensor capSensor2 = CapacitiveSensor(4, 8);
+int threshold = 25000;
 
-//electromagnet 
-#define ENABLE 6 //greenwire 6
-#define DIRA 3 //orangewire
-#define DIRB 7 //yellowwire 7
+//electromagnet
+#define ENABLE 6  //greenwire 6
+#define DIRA 3    //orangewire
+#define DIRB 7    //yellowwire 7
 int i;
 
 void setup() {
   Serial.begin(9600);
-  
-  //electromagnet set up
-  pinMode(ENABLE,OUTPUT);
-  pinMode(DIRA,OUTPUT);
-  pinMode(DIRB,OUTPUT);
-  
-  while ( status != WL_CONNECTED) {
+  stopChime();
+  //sensor setup
+  capSensor.capacitiveSensor(1);  // allow one calibration
+  capSensor2.capacitiveSensor(1);  // allow one calibration
+ //electromagnet setup
+  pinMode(ENABLE, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
+
+  while (status != WL_CONNECTED) {
     Serial.print("=> Attempting to connect to Network named: ");
-    Serial.println(ssid);            // print the network name (SSID)
+    Serial.println(ssid);  // print the network name (SSID)
     status = WiFi.begin(ssid, pass);
   }
 
-  Serial.print("=> SSID: ");         // print the SSID of the network
+  Serial.print("=> SSID: ");  // print the SSID of the network
   Serial.println(WiFi.SSID());
   IPAddress ip = WiFi.localIP();
-  Serial.print("=> IP Address: ");   // print your Arduino's IP address
+  Serial.print("=> IP Address: ");  // print your Arduino's IP address
   Serial.println(ip);
 }
 
 void loop() {
   Serial.println("=> Starting WebSocket client");
   client.begin();
- 
+
   while (client.connected()) {
-     getSensorData();
+    getSensorData();
     int messageSize = client.parseMessage();  // check if a message is available
     if (messageSize > 0) {
       String input = client.readString();
       for (int i = 0; i < input.length(); i++) {
-        if (input.substring(i, i+1) == ",") {
+        if (input.substring(i, i + 1) == ",") {
           socketdata[counter] = input.substring(lastIndex, i);
           lastIndex = i + 1;
           counter++;
@@ -73,60 +78,65 @@ void loop() {
           socketdata[counter] = input.substring(lastIndex, i);
         }
       }
-      Serial.print("Rcvd: ");    // confirm receipt of data with the computer
+      Serial.print("Rcvd: ");  // confirm receipt of data with the computer
       input.trim();
       Serial.println(input);
       input = "";
       counter = 0;
       lastIndex = 0;
-      if(socketdata[0]=="ON"){ ringChime();}
-        else{stopChime();}
+      if (socketdata[0] == "ON") {
+        ringChime();
+      } else {
+        stopChime();
+      }
     }
   }
 
   Serial.println("=> Disconnected");
-  delay(1000);
+  delay(100);
 }
-void getSensorData(){
+void getSensorData() {
   // store the value reported by the sensor in a variable
-  long sensorValue = capSensor.capacitiveSensor(30);
+  float sensorValue = capSensor.capacitiveSensor(30);
+  float sensorValue2 = capSensor2.capacitiveSensor(30);
   // if the value is greater than the threshold
-  if (sensorValue > threshold) {
+  Serial.println(sensorValue);
+  if (sensorValue > threshold && sensorValue2 > threshold) {
     client.beginMessage(TYPE_TEXT);
-      client.print("ON");      // send data
-      client.print(",");
-      client.print("0");
-      client.print(",");
-      client.println(sensorValue);
-      client.endMessage();
+    client.print("ON");  // send data
+    client.print(",");
+    client.print(sensorValue);
+    client.print(",");
+    client.println(sensorValue2);
+    client.endMessage();
   }
   // if it's lower than the threshold
   else {
-     client.beginMessage(TYPE_TEXT);
-      client.print("OFF");      // send data
-      client.print(",");
-      client.print("0");
-      client.print(",");
-      client.println("0");
-      client.endMessage();
+    client.beginMessage(TYPE_TEXT);
+    client.print("OFF");  // send data
+    client.print(",");
+    client.print("0");
+    client.print(",");
+    client.println("0");
+    client.endMessage();
   }
-  delay(10);
 }
-void ringChime(){
+void ringChime() {
   //turn on/off electromagnet
   //update to set intensity based on threshold
-   digitalWrite(ENABLE,HIGH); // enable on
-    for (i=0;i<5;i++) {
-    digitalWrite(DIRA,HIGH); //one way
-    digitalWrite(DIRB,LOW);
-    delay(20);
-    digitalWrite(DIRA,LOW);  //reverse
-    digitalWrite(DIRB,HIGH);
-    delay(20);
+  digitalWrite(ENABLE, HIGH);  // enable on
+  for (i = 0; i < 5; i++) {
+    digitalWrite(DIRA, HIGH);  //one way
+    digitalWrite(DIRB, LOW);
+    delay(80);
+    digitalWrite(DIRA, LOW);  //reverse
+    digitalWrite(DIRB, HIGH);
+    delay(80);
   }
+    digitalWrite(ENABLE,LOW); // disable
+  delay(10);
 }
-void stopChime(){
+void stopChime() {
   //turn on/off electromagnet
-  digitalWrite(ENABLE,LOW); // disable
+  digitalWrite(ENABLE, LOW);  // disable
 }
-   
